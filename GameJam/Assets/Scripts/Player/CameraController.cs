55376710c68f8c4dcbc3b091eaed3efc3f31dd33
Aedrinios <clementclerc2@gmail.com>
2020-06-26 21:49:18 +0200

    WIP Controller
    Camera controller done
    WIP Movement Controller

---
+++ GameJam/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform cameraHolder;
+
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] [Range(-90, 90)] private float minAngle, maxAngle;
+    private float xRotation = 0f;
+
+    private Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateWithMouse();
+    }
+
+    void RotateWithMouse()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime   ;
+
+        xRotation += -mouseY;
+        xRotation = Mathf.Clamp(xRotation, minAngle, maxAngle);
+
+        cameraHolder.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ GameJam/Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+    }
+}
